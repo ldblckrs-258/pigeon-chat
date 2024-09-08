e83fbdc6b10b9fbcf92a35ff7174d2fd94e17fc3
ldb-258 <102220094@sv1.dut.udn.vn>
2024-09-08 23:17:01 +0700

    refactor: Update server setup to include socket.io

---
+++ backend/src/socket/socket.js
@@ -0,0 +1,25 @@
+const onlineUsers = new Set()
+
+const handleSocketConnection = (io) => {
+  io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id)
+    onlineUsers.add(socket.id)
+    io.emit('onlineUsers', Array.from(onlineUsers))
+
+    // Handle custom events
+    socket.on('message', (data) => {
+      console.log('Message received:', data)
+      // send message to last user in onlineUsers
+      const lastUser = Array.from(onlineUsers).pop()
+      io.to(lastUser).emit('message', data)
+    })
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+      console.log('A user disconnected:', socket.id)
+      onlineUsers.delete(socket.id)
+    })
+  })
+}
+
+export default handleSocketConnection
