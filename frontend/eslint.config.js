e4b7fee0b4a45e7a1f74b10715b17823d93f2ec7
ldb-258 <102220094@sv1.dut.udn.vn>
2024-09-09 15:32:50 +0700

    fix: frontend prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,9 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.eslintrc.js'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended,     'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier'
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,21 +27,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-          printWidth: 80,
-          semi: false,
-          useTabs: true,
-          tabWidth: 2,
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': 0,
     },
   },
 )
