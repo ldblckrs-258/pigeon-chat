ab83b12341381e7b68b22b713cf49cb06c0a7c90
ldb-258 <102220094@sv1.dut.udn.vn>
2024-10-22 18:01:02 +0700

    Revert "fix: https cookie"
    This reverts commit 47d2d203b0ec9ed8b5b2561a8486d21859e724cc.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,7 +82,6 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
       session: !isRemember,
       ...(isRemember && { maxAge: 30 * 24 * 60 * 60 * 1000 }),
     })
