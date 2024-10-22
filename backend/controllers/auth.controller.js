2bef40f920cccdd5b1c4717647f626cfddb5e34b
S0me1 <128614775+ldblckrs-258@users.noreply.github.com>
2024-10-22 16:17:42 +0700

    fix: https cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,6 +82,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
+      secure: true,
       session: !isRemember,
       ...(isRemember && { maxAge: 30 * 24 * 60 * 60 * 1000 }),
     })
